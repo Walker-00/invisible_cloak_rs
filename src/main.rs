6aed8da41b5220a56602c072f0ac836df658d4c0
Walker-00 <w000alker@gmail.com>
2023-01-23 09:54:17 +0630

    Fixing Codes

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,37 @@
-use ndarray as nd;
-use opencv as cv2;
+use ndarray;
+use opencv;
 use opencv::prelude::*;
 
 const SENST: i8 = 20;
 const H_VALUE: i8 = 20;
+const KERNEL_SIZE: i8 = 10;
 
 fn detect_blue(frame: Mat, background: Mat) {
-    let mut hsv_image = cv2::gapi::GMat::default().unwrap();
-    cv2::imgproc::cvt_color(
-        &frame,
-        &mut hsv_image.into(),
-        cv2::imgproc::COLOR_RGB2HSV,
-        0,
-    );
+    let mut hsv_image = Mat::default();
+    opencv::imgproc::cvt_color(&frame, &mut hsv_image, opencv::imgproc::COLOR_RGB2HSV, 0);
 
-    let light_blue = nd::array![H_VALUE - SENST, 60, 60];
-    let dark_blue = nd::array![H_VALUE + SENST, 255, 255];
+    let light_blue = opencv::gapi::GScalar::new(opencv::core::VecN::new(
+        (H_VALUE - SENST) as f64,
+        60.,
+        60.,
+        0.,
+    ))
+    .unwrap();
 
-    let mask = cv2::gapi::in_range(&mut hsv_image, light_blue, dark_blue);
+    let dark_blue = opencv::gapi::GScalar::new(opencv::core::VecN::new(
+        (H_VALUE + SENST) as f64,
+        255.,
+        255.,
+        0.,
+    ))
+    .unwrap();
+    unsafe {
+        let mask = opencv::gapi::in_range(
+            &mut (opencv::gapi::GMat::from_raw(hsv_image.as_raw_mut())),
+            &light_blue,
+            &dark_blue,
+        );
+    }
 }
 
 fn main() {}
