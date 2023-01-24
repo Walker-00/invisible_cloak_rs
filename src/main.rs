4aab33e91290b235f9ea15cd8f43a6ad3c51c64d
Walker-00 <w000alker@gmail.com>
2023-01-24 16:02:09 +0630

    Remove unneeded unsafe block

--- src/main.rs
+++ src/main.rs
@@ -22,68 +22,66 @@ fn detect_blue(frame: &mut Mat, background: Mat) {
 
     let dark_blue = opencv::core::VecN::new((H_VALUE + SENST) as f64, 255., 255., 0.);
 
-    unsafe {
-        let mut frame = (&mut (frame.clone())).clone();
-        let mut mask = Mat::default();
+    let mut frame = (&mut (frame.clone())).clone();
+    let mut mask = Mat::default();
 
-        in_range(&mut hsv_image, &light_blue, &dark_blue, &mut mask).unwrap();
+    in_range(&mut hsv_image, &light_blue, &dark_blue, &mut mask).unwrap();
 
-        let kernel = Mat::ones(KERNEL_SIZE, KERNEL_SIZE, CV_8U).unwrap();
+    let kernel = Mat::ones(KERNEL_SIZE, KERNEL_SIZE, CV_8U).unwrap();
 
-        let mut closing = Mat::default();
+    let mut closing = Mat::default();
 
-        morphology_ex(
-            &mask,
-            &mut closing,
-            MORPH_CLOSE,
-            &kernel,
-            Point::new(-1, -1),
-            1,
-            BORDER_CONSTANT,
-            morphology_default_border_value().unwrap(),
-        )
+    morphology_ex(
+        &mask,
+        &mut closing,
+        MORPH_CLOSE,
+        &kernel,
+        Point::new(-1, -1),
+        1,
+        BORDER_CONSTANT,
+        morphology_default_border_value().unwrap(),
+    )
+    .unwrap();
+
+    let mut contours = Mat::default();
+
+    find_contours(
+        &closing,
+        &mut contours,
+        RETR_EXTERNAL,
+        CHAIN_APPROX_SIMPLE,
+        Point::new(-1, -1),
+    )
+    .unwrap();
+
+    let cont_sorted_vec: Vec<opencv::core::Point_<i32>> =
+        contours.iter::<f32>().unwrap().map(|c| c.0).collect();
+    let mut contour_mask = Mat::zeros_nd(&[500, 500, 3], CV_8U)
+        .unwrap()
+        .to_mat()
         .unwrap();
 
-        let mut contours = Mat::default();
+    let mat_cont_sorted = Mat::from_slice(&cont_sorted_vec).unwrap();
 
-        find_contours(
-            &closing,
-            &mut contours,
-            RETR_EXTERNAL,
-            CHAIN_APPROX_SIMPLE,
-            Point::new(-1, -1),
-        )
-        .unwrap();
+    fill_poly(
+        &mut contour_mask,
+        &mat_cont_sorted,
+        Scalar_::from((255., 255., 255.)),
+        LINE_8,
+        0,
+        Point::default(),
+    )
+    .unwrap();
 
-        let cont_sorted_vec: Vec<opencv::core::Point_<i32>> =
-            contours.iter::<f32>().unwrap().map(|c| c.0).collect();
-        let mut contour_mask = Mat::zeros_nd(&[500, 500, 3], CV_8U)
-            .unwrap()
-            .to_mat()
-            .unwrap();
-
-        let mat_cont_sorted = Mat::from_slice(&cont_sorted_vec).unwrap();
-
-        fill_poly(
-            &mut contour_mask,
-            &mat_cont_sorted,
-            Scalar_::from((255., 255., 255.)),
-            LINE_8,
-            0,
-            Point::default(),
-        )
-        .unwrap();
-
-        fill_poly(
-            &mut frame,
-            &mat_cont_sorted,
-            Scalar_::from((0., 0., 0.)),
-            LINE_8,
-            0,
-            Point::default(),
-        )
-        .unwrap();
-    }
+    fill_poly(
+        &mut frame,
+        &mat_cont_sorted,
+        Scalar_::from((0., 0., 0.)),
+        LINE_8,
+        0,
+        Point::default(),
+    )
+    .unwrap();
 }
 
 fn main() {}
